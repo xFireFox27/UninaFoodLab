8ee7b70997666610a70a73bb7a80ec115b5f4d7c
Niccolò Davide Cavallo <niccolocavallo@gmail.com>
2025-07-08 17:41:36 +0200

    Modifica InsertChef

--- Progetto Basi di Dati/SQL/Insert/InsertChef.sql
+++ Progetto Basi di Dati/SQL/Insert/InsertChef.sql
@@ -1,11 +1,11 @@
 INSERT INTO Chef(Nome, Cognome, Username, Password)
 VALUES
-    ('Christian', 'Ranavolo', 'Nerdone11', 'gdH1_u79*df'),
     ('Giuseppe', 'Esposito', 'ChefPeppe', 'c3F!9d2@kL'),
     ('Maria', 'Rossi', 'ChefMaria', 'm4R#8s5*eQ'),
     ('Antonio', 'Bianchi', 'ChefAntonio', 'a7T$2g6!hV'),
     ('Francesca', 'Verdi', 'ChefFrancesca', 'f9C&3j8@lX'),
     ('Luca', 'Gallo', 'ChefLuca', 'l5K*1p4#zY'),
+    ('Christian', 'Ranavolo', 'Il_Nerdone_11', 'gdH1_u79*df'),
     ('Elena', 'Martini', 'ChefElena', 'e2M!6t3@qZ'),
     ('Marco', 'Conti', 'ChefMarco', 'm8N#4v5*eW'),
     ('Sara', 'Ferrari', 'ChefSara', 's1F$7b2!hR'),
