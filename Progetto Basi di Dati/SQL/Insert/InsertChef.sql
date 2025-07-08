90f5ba1f9fbea837e609b780992b8462bd0ad228
Niccolò Davide Cavallo <niccolocavallo@gmail.com>
2025-07-08 17:29:58 +0200

    Inserimento chef;

--- Progetto Basi di Dati/SQL/Insert/InsertChef.sql
+++ Progetto Basi di Dati/SQL/Insert/InsertChef.sql
@@ -5,4 +5,8 @@ VALUES
     ('Maria', 'Rossi', 'ChefMaria', 'm4R#8s5*eQ'),
     ('Antonio', 'Bianchi', 'ChefAntonio', 'a7T$2g6!hV'),
     ('Francesca', 'Verdi', 'ChefFrancesca', 'f9C&3j8@lX'),
-    ('Luca', 'Gallo', 'ChefLuca', 'l5K*1p4#zY');
+    ('Luca', 'Gallo', 'ChefLuca', 'l5K*1p4#zY'),
+    ('Elena', 'Martini', 'ChefElena', 'e2M!6t3@qZ'),
+    ('Marco', 'Conti', 'ChefMarco', 'm8N#4v5*eW'),
+    ('Sara', 'Ferrari', 'ChefSara', 's1F$7b2!hR'),
+    ('Alessandro', 'Romano', 'ChefAle', 'a3L&9d6@kT');
