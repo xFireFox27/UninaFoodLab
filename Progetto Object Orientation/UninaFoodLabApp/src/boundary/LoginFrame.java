a9c5b8d76e658a897fbb58d15f322d8c4cc6e1ec
Carmine Amato <carmineam.04@gmail.com>
2025-07-12 17:15:08 +0200

    Add files via upload

---
+++ Progetto Object Orientation/UninaFoodLabApp/src/boundary/LoginFrame.java
@@ -0,0 +1,27 @@
+package boundary;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class LoginFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	
+	/**
+	 * Create the frame.
+	 */
+	public LoginFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+
+	}
+
+}
---
+++ Progetto Object Orientation/UninaFoodLabApp/src/control/Controller.java
@@ -0,0 +1,10 @@
+package control;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.IOException;
+
+public class Controller {
+
+}
