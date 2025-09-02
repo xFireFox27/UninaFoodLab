8ad60fa8ff6bc7382aecdb58914b280c5539d6b9
Bruno Capuozzo <brunocapuozzo1@gmail.com>
2025-09-02 17:49:55 +0200

    minime modifiche

--- Progetto Object Orientation/UninaFoodLabApp/src/boundary/HomepageChef.java
+++ Progetto Object Orientation/UninaFoodLabApp/src/boundary/HomepageChef.java
@@ -21,7 +21,7 @@ public class HomepageChef extends JFrame {
 		theController = c;
 		setVisible(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 900, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
