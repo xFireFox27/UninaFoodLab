3077c3cb62b8bd9b71523b9aaf05a571e081c84c
Niccolò Davide Cavallo <niccolocavallo@gmail.com>
2025-09-06 17:37:14 +0200

    Indentazione

--- Progetto Object Orientation/UninaFoodLabApp/src/DAO/NotificaDAO.java
+++ Progetto Object Orientation/UninaFoodLabApp/src/DAO/NotificaDAO.java
@@ -10,7 +10,7 @@ import entity.Notifica;
 import entity.Chef;
 import entity.Corso;
 
-public class NotificaDAO implements NotificaDaoInterface{
+public class NotificaDAO implements NotificaDaoInterface {
 	
 	@Override
 	public List<Notifica> getNotificheByChef(Chef chef) throws SQLException {
@@ -21,7 +21,7 @@ public class NotificaDAO implements NotificaDaoInterface{
 					 "WHERE c.username = ?";
 		
 		try (Connection connection = DB.getConnection();
-			PreparedStatement stmt = connection.prepareStatement(sql)){
+			PreparedStatement stmt = connection.prepareStatement(sql)) {
 				stmt.setString(1, chef.getUsername());
 				try(ResultSet rs = stmt.executeQuery()){
 					while(rs.next()) {
@@ -44,82 +44,73 @@ public class NotificaDAO implements NotificaDaoInterface{
 		String sql = "INSERT INTO notifica (usernamechef, oggetto, testo) VALUES(?, ?, ?) ";
 		
 		try (Connection connection = DB.getConnection();
-			 PreparedStatement st = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
-			
-			st.setString(1, chef.getUsername());
-			st.setString(2, oggetto);
-			st.setString(3, testo);
-			
-			int insertRows = st.executeUpdate();
-			if(insertRows > 0) {
-				Notifica notifica = new Notifica(chef, oggetto, testo, new Timestamp(System.currentTimeMillis()));
-				String sql2 = "INSERT INTO riceve (idnotifica, usernameutente) " +
-						 "SELECT idnotifica, usernameutente " +
-						 "FROM notifica AS n " +
-						 "JOIN corso AS c ON c.usernamechef = n.usernamechef " +
-						 "JOIN iscrizione AS i ON c.idcorso = i.idcorso " +
-						 "WHERE n.idnotifica = ? AND c.usernamechef = ? AND c.titolo = ? AND c.anno = ?";
-				try(ResultSet generatedKey = st.getGeneratedKeys();
-					PreparedStatement st2 = connection.prepareStatement(sql2)){
-					if(generatedKey.next()) {
-						int idNotifica = generatedKey.getInt(1);
-						
-							st2.setInt(1, idNotifica);
-							st2.setString(2,chef.getUsername());
-							st2.setString(3, corso.getTitolo());
-							st2.setInt(4, corso.getAnnoFrequenza());
-							st2.executeUpdate();
+			PreparedStatement st = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+				st.setString(1, chef.getUsername());
+				st.setString(2, oggetto);
+				st.setString(3, testo);
+				int insertRows = st.executeUpdate();
+				if(insertRows > 0) {
+					Notifica notifica = new Notifica(chef, oggetto, testo, new Timestamp(System.currentTimeMillis()));
+					String sql2 = "INSERT INTO riceve (idnotifica, usernameutente) " +
+									 "SELECT idnotifica, usernameutente " +
+									 "FROM notifica AS n " +
+									 "JOIN corso AS c ON c.usernamechef = n.usernamechef " +
+									 "JOIN iscrizione AS i ON c.idcorso = i.idcorso " +
+									 "WHERE n.idnotifica = ? AND c.usernamechef = ? AND c.titolo = ? AND c.anno = ?";
+					try(ResultSet generatedKey = st.getGeneratedKeys();
+						PreparedStatement st2 = connection.prepareStatement(sql2)){
+							if(generatedKey.next()) {
+								int idNotifica = generatedKey.getInt(1);
+								
+									st2.setInt(1, idNotifica);
+									st2.setString(2,chef.getUsername());
+									st2.setString(3, corso.getTitolo());
+									st2.setInt(4, corso.getAnnoFrequenza());
+									st2.executeUpdate();
+							} else {
+								return null;
+							}
+							return notifica;
 					}
-					else {
-						return null;
-					}
-					return notifica;
+				} else {
+					return null;
 				}
-			}
-			else {
-				return null;
-			}
 		}
 	}
 
 	@Override
 	public Notifica inviaNotificaATuttiICorsi(Chef chef, String oggetto, String testo) throws SQLException {
-		String sql = "INSERT INTO notifica (usernamechef, oggetto, testo) " +
-			     "VALUES(?, ?, ?) ";
-	try (Connection connection = DB.getConnection();
-		 PreparedStatement st = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
-		
-		st.setString(1, chef.getUsername());
-		st.setString(2, oggetto);
-		st.setString(3, testo);
-		
-		int insertRows = st.executeUpdate();
-		if(insertRows > 0) {
-			Notifica notifica = new Notifica(chef, oggetto, testo, new Timestamp(System.currentTimeMillis()));
-			String sql2 = "INSERT INTO riceve (idnotifica, usernameutente) " +
-					 "SELECT idnotifica, usernameutente " +
-					 "FROM notifica AS n " +
-					 "JOIN corso AS c ON c.usernamechef = n.usernamechef " +
-					 "JOIN iscrizione AS i ON c.idcorso = i.idcorso " +
-					 "WHERE n.idnotifica = ? AND c.usernamechef = ?";
-			try(ResultSet generatedKey = st.getGeneratedKeys();
-				PreparedStatement st2 = connection.prepareStatement(sql2)){
-				if(generatedKey.next()) {
-					int idNotifica = generatedKey.getInt(1);
-					
-						st2.setInt(1, idNotifica);
-						st2.setString(2,chef.getUsername());
-						st2.executeUpdate();
-				}
-				else {
+		String sql = "INSERT INTO notifica (usernamechef, oggetto, testo) VALUES(?, ?, ?) ";
+		try (Connection connection = DB.getConnection();
+			PreparedStatement st = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+				st.setString(1, chef.getUsername());
+				st.setString(2, oggetto);
+				st.setString(3, testo);
+				int insertRows = st.executeUpdate();
+				if(insertRows > 0) {
+					Notifica notifica = new Notifica(chef, oggetto, testo, new Timestamp(System.currentTimeMillis()));
+					String sql2 = "INSERT INTO riceve (idnotifica, usernameutente) " +
+									 "SELECT idnotifica, usernameutente " +
+									 "FROM notifica AS n " +
+									 "JOIN corso AS c ON c.usernamechef = n.usernamechef " +
+									 "JOIN iscrizione AS i ON c.idcorso = i.idcorso " +
+									 "WHERE n.idnotifica = ? AND c.usernamechef = ?";
+					try(ResultSet generatedKey = st.getGeneratedKeys();
+						PreparedStatement st2 = connection.prepareStatement(sql2)) {
+							if(generatedKey.next()) {
+								int idNotifica = generatedKey.getInt(1);
+								st2.setInt(1, idNotifica);
+								st2.setString(2,chef.getUsername());
+								st2.executeUpdate();
+							} else {
+								return null;
+							}
+							return notifica;
+					}
+				} else {
 					return null;
 				}
-				return notifica;
-			}
-		}
-		else {
-			return null;
 		}
 	}
-	}
+	
 }
